c70d0f5e69c74c821198bad5b6de4b12f36a3e2c
Enric Sala <sinh32.enric@gmail.com>
2015-11-06 21:07:45 +0100

    Fix wrong request mappings

--- src/main/java/com/gemavi/backend/controller/CompanyController.java
+++ src/main/java/com/gemavi/backend/controller/CompanyController.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gemavi.backend.domain.Company;
 import com.gemavi.backend.service.CompanyService;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api/company")
 public class CompanyController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CompanyController.class);
@@ -17,7 +19,7 @@ public class CompanyController {
 	@Autowired
 	private CompanyService companyService;
 
-	@RequestMapping(value = "/company/{companyId}")
+	@RequestMapping(value = "/{companyId}", method = RequestMethod.GET)
 	public Company getCompany(String companyId) {
 		LOG.info("requested company by id {}", companyId);
 		return companyService.getCompany(companyId).orElse(null);
--- src/main/java/com/gemavi/backend/controller/CountController.java
+++ src/main/java/com/gemavi/backend/controller/CountController.java
@@ -1,5 +1,7 @@
 package com.gemavi.backend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,19 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gemavi.backend.domain.Office;
 import com.gemavi.backend.service.CheckinService;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api/counter")
 public class CountController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(CountController.class);
 
 	@Autowired
 	private CheckinService checkins;
 
-	@RequestMapping(value = "/counter/{officeId}/enter")
+	@RequestMapping(value = "/{officeId}/enter")
 	public Office enter(@PathVariable String officeId) {
+		LOG.info("a visitor entered office={}", officeId);
 		return checkins.addVisitor(officeId);
 	}
 
-	@RequestMapping(value = "/counter/{officeId}/leave")
+	@RequestMapping(value = "/{officeId}/leave")
 	public Office leave(@PathVariable String officeId) {
+		LOG.info("a visitor left office={}", officeId);
 		return checkins.removeVisitor(officeId);
 	}
 
