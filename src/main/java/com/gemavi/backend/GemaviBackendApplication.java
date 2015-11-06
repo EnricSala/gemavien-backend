6f6269d213d5af35cacce8a3c7ad26fa96f9f3b3
Enric Sala <sinh32.enric@gmail.com>
2015-11-06 22:00:06 +0100

    Remove bad whitespaces

--- src/main/java/com/gemavi/backend/GemaviBackendApplication.java
+++ src/main/java/com/gemavi/backend/GemaviBackendApplication.java
@@ -2,11 +2,13 @@ package com.gemavi.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class GemaviBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GemaviBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GemaviBackendApplication.class, args);
+	}
 }
